3e3d504b0e518ba75cae7e7e3f06ac2fef047766
ReponseD <155183567+ReponseD@users.noreply.github.com>
2025-05-08 19:14:02 +0200

    Update display_interface.ino

--- firmware/display_interface.ino
+++ firmware/display_interface.ino
@@ -1,1 +1,19 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+}
+
+void loop() {
+  int gas = analogRead(A0);
+  int temp = analogRead(A1);
+  int water = analogRead(A2);
+  Serial.print(gas);
+  Serial.print(",");
+  Serial.print(temp);
+  Serial.print(",");
+  Serial.println(water);
+  delay(500);
+}
 
